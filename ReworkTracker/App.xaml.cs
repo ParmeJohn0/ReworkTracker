726fb499bdfe0c0da0aad3405c645eff68442d4d
ParmeJohn0 <emmettparmenter@gmail.com>
2024-01-23 08:45:40 -0500

    Add project files.

---
+++ ReworkTracker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ReworkTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
