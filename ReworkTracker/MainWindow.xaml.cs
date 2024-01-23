f73ceb21163042d36b4cfeb8dfa825a65308b4df
ParmeJohn0 <emmettparmenter@gmail.com>
2024-01-23 15:17:29 -0500

    Front end dev

--- ReworkTracker/MainWindow.xaml.cs
+++ ReworkTracker/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ReworkTracker
 {
@@ -19,12 +20,22 @@ namespace ReworkTracker
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            txtTime.Text = DateTime.Now.ToLongTimeString();
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
         }
+        public string Time = DateTime.Now.ToString("HH:mm:ss");
         void Close_Click(object sender, RoutedEventArgs e) => Close();
         void Min_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
         void Max_Click(object sender, RoutedEventArgs e)
@@ -48,5 +59,17 @@ namespace ReworkTracker
                 }
             }
         }
+        void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+        void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            //clear all the fields in the form
+
+        }
+        void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //call the SQL statement to insert the data into the database
+
+        }
+
     }
 }
